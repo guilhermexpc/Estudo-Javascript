734bd2c987662976b507e0bbb9b41715935eb26d
Guilherme Campos <guilhermee1219@gmail.com>
2026-08-31 17:56:59 -0300

    add variabels

---
+++ scripts/variables.js
@@ -0,0 +1,14 @@
+var user; // undefined variable
+console.log(user);
+
+var fullName = "Guilherme Campos";
+console.log(fullName);
+
+fullName = "Guilherme PC";
+console.log(fullName);
+
+let email = "guilherme@gmail.com";
+// let email = doesn't work
+
+// Const value doesn't change
+const rg = "10.215.369-5";
