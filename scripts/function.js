9429022a4277239a5e408e3a6999714db50a85ed
Guilherme Campos <guilhermee1219@gmail.com>
2026-08-31 22:14:52 -0300

    refactor: enhance function.js with additional examples and callback implementations

--- scripts/function.js
+++ scripts/function.js
@@ -1,6 +1,43 @@
 // Função anônima - Função sem nome
 const showMessage = function () {
+  var teste = "asdas";
   return "Função sem nome";
 };
 
-console.log(showMessage);
+console.log(teste);
+console.log(showMessage());
+
+// Arrow function
+const greatingMenssage = (userName) => {
+  // return "Greetings " + userName;
+  return `Greetings + ${userName}`;
+};
+
+console.log(greatingMenssage("Jhon"));
+
+// Callback Function: Passa uma função para outra função como Argumento
+function executeFunction(regularParam, callback) {
+  console.log(`Regular function execution: ${regularParam}`);
+  callback();
+}
+
+function callbackFunction() {
+  console.log("Another Function executing");
+}
+
+executeFunction("callback Teste", callbackFunction);
+
+// Criando a função no proprio parâmetro
+executeFunction("callback Teste 2", function () {
+  console.log("Another Function executing on Params");
+});
+
+// Utilizando Arrow Function
+executeFunction("callback Teste 3", () => {
+  console.log("Another Function executing on Params with arrow function");
+});
+
+// Utilizando Arrow Function de uma linha
+executeFunction("callback Teste 3", () =>
+  console.log("Another Function executing on Params with arrow function - singles line")
+);
